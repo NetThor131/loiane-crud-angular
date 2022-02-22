0f5fe54de2dbcf8982fe3e77075efc5244f5d342
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2022-02-22 10:03:51 -0300

    Adicionar tabela de cursos ao componente de curso

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { CursosComponent } from './cursos';
 import { CursosRoutingModule } from './cursos-routing.module';
 
 @NgModule({
   declarations: [CursosComponent],
-  imports: [CommonModule, CursosRoutingModule],
+  imports: [CommonModule, CursosRoutingModule, MatTableModule],
 })
 export class CursosModule {}
--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Curso } from '../models';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
+  cursos: Curso[] = [
+    {
+      _id: '1',
+      nome: 'Angular',
+      categoria: 'front-end',
+    },
+  ];
+  displayedColumns = ['nome', 'categoria'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

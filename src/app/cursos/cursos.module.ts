01e376886ae0da1acf72f192865e3d37f25d0270
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2022-02-22 10:18:57 -0300

    Adicionar novo modulo no modulo de cursos

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material';
 import { CursosComponent } from './cursos';
 import { CursosRoutingModule } from './cursos-routing.module';
 
 @NgModule({
   declarations: [CursosComponent],
-  imports: [CommonModule, CursosRoutingModule, MatTableModule],
+  imports: [CommonModule, CursosRoutingModule, AppMaterialModule],
 })
 export class CursosModule {}
